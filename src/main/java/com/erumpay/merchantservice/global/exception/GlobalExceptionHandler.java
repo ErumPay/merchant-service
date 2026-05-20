1de88a4ae613b74bde1164fe808c7cc429ea77d9
choboreum <choboreum@naver.com>
2026-05-20 14:54:55 +0900

    [KAN-1179] chore: 예외 처리 로깅 추가

--- src/main/java/com/erumpay/merchantservice/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/erumpay/merchantservice/global/exception/GlobalExceptionHandler.java
@@ -1,16 +1,20 @@
 package com.erumpay.merchantservice.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateMerchantException.class)
     public ResponseEntity<String> handleDuplicateMerchantException(
             DuplicateMerchantException e) {
+
+        log.error("중복된 사업자번호 등록 요청 발생", e);
 
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(e.getMessage());
@@ -19,6 +23,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MerchantNotFoundException.class)
     public ResponseEntity<String> handleMerchantNotFoundException(
             MerchantNotFoundException e) {
+
+        log.error("가맹점 조회 실패", e);
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(e.getMessage());
